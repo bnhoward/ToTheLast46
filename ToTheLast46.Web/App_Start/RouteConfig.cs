df9f5391d1f62902c993e361c6598faa2cc045c8
Barry Howard <barrynhoward@gmail.com>
2016-01-10 16:56:27 +0000

    Fonts added. Need to remove all but 1 or 2

--- ToTheLast46.Web/App_Start/RouteConfig.cs
+++ ToTheLast46.Web/App_Start/RouteConfig.cs
@@ -12,12 +12,13 @@ namespace ToTheLast46.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute(null, "band", new { controller = "Home", action = "Band" });
+            routes.MapRoute(null, "blog", new { controller = "Home", action = "Blog" });
+            routes.MapRoute(null, "gigs", new { controller = "Home", action = "Gigs" });
+            routes.MapRoute(null, "guestbook", new { controller = "Home", action = "Guestbook" });
+            routes.MapRoute(null, "gallery", new { controller = "Home", action = "Gallery" });
+            routes.MapRoute(null, "friends", new { controller = "Home", action = "Friends" });
+            routes.MapRoute(null, "{controller}/{action}/{id}", new { controller = "Home", action = "Index", id = UrlParameter.Optional });
         }
     }
 }
