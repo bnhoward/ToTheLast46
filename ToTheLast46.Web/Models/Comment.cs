763e09230f3ccca186e78db834275f31c17637a1
Barry Howard <barrynhoward@gmail.com>
2016-02-09 19:19:21 +0000

    Aesthetic updates

--- ToTheLast46.Web/Models/Comment.cs
+++ ToTheLast46.Web/Models/Comment.cs
@@ -1,13 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToTheLast46.Web.Models
 {
     public class GuestbookComment
     {
-        [Required(ErrorMessage ="Please enter your name")]
+        [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
         public string Email { get; set; }
         [Required(ErrorMessage = "Please add a comment")]
+        [DisplayName("Comment")]
         public string Com { get; set; }
     }
 }
