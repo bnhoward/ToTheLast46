8d6ea6cb51eba6601273104e5e221ae0f9e99da2
Barry Howard <barrynhoward@gmail.com>
2016-02-19 13:08:06 +0000

    Google analytics updated

---
+++ build.fsx
@@ -0,0 +1,11 @@
+﻿// define the dependencies
+module Build
+
+#r "packages/FAKE.4.20.0/tools/FakeLib.dll"
+
+open Fake
+
+Target "Test" (fun _ -> trace "Testing stuff...")
+Target "Deploy" (fun _ -> trace "Heavy deploy action")
+"Test" ==> "Deploy"
+Run "Deploy"
